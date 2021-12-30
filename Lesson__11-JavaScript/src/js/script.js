fd93c0b839122305af0af14b7be1707b997f5fcb
Danil <stratorua@gmail.com>
2021-12-30 16:50:04 +0200

    Доделать

---
+++ Lesson__11-JavaScript/src/js/script.js
@@ -0,0 +1,27 @@
+// Задание No1. Переведите текст вида border-left-width в borderLeftWidth
+
+// const camelize = (str) => {
+//   return str
+//     .split("-")
+//     .map((el, index) => {
+//       if (index == 0) {
+//         return el;
+//       } else {
+//         return el[0].toUpperCase() + el.slice(1);
+//       }
+//     })
+//     .join("");
+// };
+// console.log(camelize("-webkit-transition"));
+
+// Задание №2. Фильтрация по диапазону
+
+// let arr = [5, 3, 8, 1];
+// const filterRange = (arr, a, b) => {
+//   return arr.filter((el) => a <= el && el <= b);
+// };
+// let filtered = filterRange(arr, 1, 4);
+// console.log(filtered);
+// console.log(arr);
+
+// Задание №3 Фильтрация по диапазону «на месте»
