39666efc2ddc7eef9ca193eeb1cb404493c8b0c3
Danil <stratorua@gmail.com>
2022-01-06 19:45:16 +0200

    add special functions

--- Mini-Projects JS/Who want to be a millionare/src/js/app.js
+++ Mini-Projects JS/Who want to be a millionare/src/js/app.js
@@ -1,30 +1,34 @@
 (function app() {
-  //variaables
-
-  let btnStart = document.querySelector(".btn-start");
-  let userName = document.getElementById("userName");
-  let greeting = document.querySelector(".game__greetings");
-  let ul = document.querySelectorAll("ul li");
-  let points = document.querySelector(".points");
-  let currentMoney = 0;
+  //variables
+  const ul = document.querySelectorAll("ul li");
+  let gameEnd = document.querySelector(".game__end");
+  let restartBtn = document.querySelector(".restart__btn");
+  let earnPoints = document.querySelector(".earnPoints");
+  let timer = document.querySelector(".timer");
+  let money = "0$";
+  //special
+  let fifty = document.querySelector(".fifty-fifty");
+  let callFriend = document.querySelector(".call-friend");
 
   //Events
-
-  btnStart.addEventListener("click", () => {
-    if (userName.value == "") {
-      alert("Please Enter a name");
-    } else {
-      startGame();
-      points.classList.remove("hide");
-    }
+  fifty.addEventListener("click", () => {
+    fiftyOption();
   });
 
+  callFriend.addEventListener("click", () => {
+    friendOption();
+  });
+
+  window.addEventListener("load", startGame());
+
+  //FUNCTIONS
+
   function startGame() {
-    greeting.classList.add("hide");
     init();
   }
 
   //ПОДПРАВИТЬ
+
   function init() {
     // (B1) WRAPPER
     quiz.hWrap = document.getElementById("quizWrap");
@@ -45,20 +49,20 @@
 
   function draw() {
     // (C1) QUESTION
-    quiz.hQn.innerHTML = quiz.data[quiz.now].question;
+    quiz.hQn.textContent = quiz.data[quiz.now].question;
 
     // (C2) OPTIONS
-    quiz.hAns.innerHTML = "";
+    quiz.hAns.textContent = "";
 
     for (let i in quiz.data[quiz.now].option) {
       let radio = document.createElement("input");
       radio.type = "radio";
       radio.name = "quiz";
-      radio.id = "quizo" + i;
+      radio.id = "quiz" + i;
       quiz.hAns.appendChild(radio);
       let label = document.createElement("label");
-      label.innerHTML = quiz.data[quiz.now].option[i];
-      label.setAttribute("for", "quizo" + i);
+      label.textContent = quiz.data[quiz.now].option[i];
+      label.setAttribute("for", "quiz" + i);
       label.dataset.idx = i;
       label.addEventListener("click", () => {
         select(label);
@@ -73,56 +77,79 @@
     for (let label of all) {
       label.removeEventListener("click", quiz.select);
     }
+
     // (D2) CHECK IF CORRECT
     let correct = option.dataset.idx == quiz.data[quiz.now].answer;
     if (correct) {
       quiz.score++;
       option.classList.add("correct");
-      console.log("correct");
 
       for (let i = 0; i < ul.length; i++) {
         if (!ul[ul.length - 1 - i].classList.contains("orange")) {
           ul[ul.length - 1 - i].classList.add("orange");
-          currentMoney = ul[ul.length - 1 - i].textContent;
+          money = ul[ul.length - 1 - i].textContent;
           break;
         }
       }
     } else {
       option.classList.add("wrong");
-      console.log("wrong");
       gameOver();
     }
+
     // (D3) NEXT QUESTION OR END GAME
     quiz.now++;
     setTimeout(() => {
       if (quiz.now < quiz.data.length) {
-        if (quiz.now === 5) {
-          console.log("ЭТО ПЯТЫЙ ВОПРОС!"); //! Настроить несгораемую сумму на 5, 10 вопрос.
-        }
         draw();
       } else {
-        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
-        quiz.hAns.innerHTML = "";
+        quiz.hQn.textContent = `Вы ответили на ${quiz.score} из ${quiz.data.length} правильно и заработали ${money}.`;
+        quiz.hAns.textContent = "";
       }
     }, 1000);
   }
 
-  // (E) RESTART QUIZ
-  function reset() {
-    quiz.now = 0;
-    quiz.score = 0;
-    draw();
-  }
-
-  // GAME OVER
-
+  //GameOver
   function gameOver() {
-    console.log("gameOver");
-    console.log(currentMoney);
+    if (quiz.now === 5) earnPoints.textContent = "1000 $";
+    if (quiz.now === 10) earnPoints.textContent = "125 000 $";
+    earnPoints.textContent = money;
+    setTimeout(() => {
+      gameEnd.classList.remove("hide");
+    }, 1000);
+
+    //RESTART BTN
+    restartBtn.addEventListener("click", () => {
+      restart();
+    });
   }
 
-  // SPECIAL FUNCTIONS
+  //RestartGame
+  function restart() {
+    callFriend.classList.remove("hide");
+    setTimeout(() => {
+      gameEnd.classList.add("hide");
+      quiz.now = 0;
+      quiz.score = 0;
+      ul.forEach((li) => li.classList.remove("orange"));
+    }, 300);
+  }
 
-  function callFriend() {}
-  function fifty() {}
+  //SPECIAL FUNC
+
+  function fiftyOption() {
+    console.log("50/50");
+  }
+
+  function friendOption() {
+    if (quiz.data[quiz.now].answer === 0) {
+      alert("A");
+    } else if (quiz.data[quiz.now].answer === 1) {
+      alert("B");
+    } else if (quiz.data[quiz.now].answer === 2) {
+      alert("C");
+    } else {
+      alert("D");
+    }
+    callFriend.classList.add("hide");
+  }
 })();
